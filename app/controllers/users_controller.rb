a437ea4ed53da8704da6940797263215bc83da4c
Oscar Michel Herrera <A00817658@itesm.mx>
2019-03-04 18:02:39 -0600

    Hotfix admin comprobe

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -74,6 +74,6 @@ class UsersController < ApplicationController
     end
 
     def comprobe_admin
-      redirect_to root_path if current_user.is_admin? if !current_user.nil?
+      redirect_to root_path if !current_user.is_admin?
     end
 end
